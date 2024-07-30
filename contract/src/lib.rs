c46799acf6c811b33b72372fdc7ccbc8e12d077b
BukiOffor <ebuka2264@yahoo.com>
2024-07-30 11:31:50 +0100

    feat: soroban init

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,12 +1,35 @@
-use soroban_sdk::{contract, contractimpl, vec, symbol_short, Env, Symbol, Vec};
+use soroban_sdk::{contract, contractimpl, contracttype, token, Address, Env};
 
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    Admin
+}
+
+fn get_balance(e: &Env, contract_id: &Address) -> i128 {
+    let client = token::Client::new(e, contract_id);
+    client.balance(&e.current_contract_address())
+}
 #[contract]
 pub struct Gonana;
 
 #[contractimpl]
 impl Gonana {
-    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
-        vec![&env, symbol_short!("Hello"), to]
+   
+    pub fn initialize(env:Env, admin: Address) {
+        env.storage().instance().set(&DataKey::Admin, &admin)
+    }
+
+    pub fn deposit(e:Env, amount:i128) {
+
+    }
+    pub fn withdraw(e:Env){
+
+    }
+    pub fn balance_of_contract(e:Env) -> i128{
+        let balance = get_balance(&e, &e.current_contract_address());
+        balance
+
     }
 }
 
@@ -14,9 +37,6 @@ impl Gonana {
 fn test() {
     let env = Env::default();
     let contract_id = env.register_contract(None, Gonana);
-    let client = GonanaClient::new(&env, &contract_id);
+    let _client = GonanaClient::new(&env, &contract_id);
 
-    let words = client.hello(&symbol_short!("Dev"));
-
-    assert_eq!(words, vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]);
 }
